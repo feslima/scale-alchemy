ca46a411c126fbce3bd02205316a4791739d50d0
Felipe <26683783+feslima@users.noreply.github.com>
2024-09-01 12:20:03 -0300

    chore: fix comment

--- interpreter/lexer.ts
+++ interpreter/lexer.ts
@@ -35,8 +35,9 @@ export class Lexer {
 
   private _parenthesisTracker: number[] = [];
 
-  /* NOTE: next char has to be one of operator, EOF or
-   * closing parenthesis to constitute a valid number */
+  /* NOTE: next char for number has to be one of operator, EOF, .
+   * or closing parenthesis to constitute a valid number
+   */
   private _validCharSetForNumber = new Set(["+", "-", "*", "/", ")", "", "."]);
 
   constructor(input: string) {
