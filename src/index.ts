3aa2e7c327e00fb31d5e15eec1c00f7a58caac90
Felipe <26683783+feslima@users.noreply.github.com>
2024-09-03 19:50:27 -0300

    feat: package index

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export { Lexer } from "./interpreter/lexer";
+export { Parser } from "./interpreter/parser";
+export {
+  ErrorValue,
+  EvaluationWithUnitEnvironmentType,
+  EvaluatorWithUnits,
+  NumberWithUnitValue
+} from "./interpreter/unit-evaluator";
+export { QuantitySytem } from "./units";
+
